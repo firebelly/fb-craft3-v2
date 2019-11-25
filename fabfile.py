d3819e0252c335762119e3bc9517c90f8ed07b83
Nate Beaty <nate@clixel.com>
2019-11-25 18:27:27 -0500

    pull in deploy setup from onward fabfile

--- fabfile.py
+++ fabfile.py
@@ -13,16 +13,10 @@ def production():
   env.remotepath = '/home/firebelly/webapps/fb_craft3'
   env.remote_protocol = 'https'
 
-def assets():
-  local('npx gulp --production')
-
-def dev():
-  local('npx gulp watch')
-
-def syncstaging():
-  with cd(env.remotepath):
-    run('/usr/bin/mysqldump --defaults-extra-file=/home/firebelly/etc/.my.cnf -u fb_craft_sql fb_craft3 | /usr/bin/mysql --defaults-extra-file=/home/firebelly/etc/.my.cnf -u fb_craft_sql fb_craft3_dev')
-    run('/usr/bin/rsync -av --delete /home/firebelly/webapps/fb_craft3/web/uploads/ /home/firebelly/webapps/fb_craft3_dev/web/uploads/')
+# def syncstaging():
+#   with cd(env.remotepath):
+#     run('/usr/bin/mysqldump --defaults-extra-file=/home/firebelly/etc/.my.cnf -u fb_craft_sql fb_craft3 | /usr/bin/mysql --defaults-extra-file=/home/firebelly/etc/.my.cnf -u fb_craft_sql fb_craft3_dev')
+#     run('/usr/bin/rsync -av --delete /home/firebelly/webapps/fb_craft3/web/uploads/ /home/firebelly/webapps/fb_craft3_dev/web/uploads/')
 
 def devsetup():
   print "Installing composer, node and bower assets...\n"
@@ -33,11 +27,15 @@ def devsetup():
   local('cp .env-example .env')
   print "OK DONE! Hello? Are you still awake?\nEdit your .env file with local credentials\nRun `npx gulp watch` to run local gulp to compile & watch assets"
 
-def deploy(composer='y'):
+def deploy(composer='y', assets='y'):
   update()
   if composer == 'y':
     composer_install()
-  # clear_cache()
+  # build and sync production assets
+  if assets != 'n':
+    local('yarn build:production')
+    run('mkdir -p ' + env.remotepath + '/web/assets/dist')
+    put('web/assets/dist', env.remotepath + '/web/assets/')
 
 def update():
   with cd(env.remotepath):
@@ -45,7 +43,4 @@ def update():
 
 def composer_install():
   with cd(env.remotepath):
-    run('php72 ~/bin/composer.phar install')
-
-# def clear_cache():
-#   run ('curl -vs -o /dev/null {0}://{1}/actions/cacheClear/clear?key=fbclear > /dev/null 2>&1'.format(env.remote_protocol, env.hosts[0]))
+    run('~/bin/composer install')
