cb7dc110467e0ca2d710735550b4519a4305364a
Nate Beaty <nate@clixel.com>
2019-11-27 12:00:06 -0500

    gulp -> yarn

--- fabfile.py
+++ fabfile.py
@@ -6,12 +6,10 @@ env.user = 'firebelly'
 env.remotepath = '/home/firebelly/webapps/fb_craft3_dev'
 env.git_branch = 'master'
 env.warn_only = True
-env.remote_protocol = 'http'
 
 def production():
   env.hosts = ['firebellydesign.com']
   env.remotepath = '/home/firebelly/webapps/fb_craft3'
-  env.remote_protocol = 'https'
 
 # def syncstaging():
 #   with cd(env.remotepath):
@@ -19,13 +17,11 @@ def production():
 #     run('/usr/bin/rsync -av --delete /home/firebelly/webapps/fb_craft3/web/uploads/ /home/firebelly/webapps/fb_craft3_dev/web/uploads/')
 
 def devsetup():
-  print "Installing composer, node and bower assets...\n"
+  print "Composing and yarning...\n"
   local('composer install')
-  local('npm install')
-  local('cd assets && bower install')
-  local('npx gulp')
-  local('cp .env-example .env')
-  print "OK DONE! Hello? Are you still awake?\nEdit your .env file with local credentials\nRun `npx gulp watch` to run local gulp to compile & watch assets"
+  local('yarn')
+  local('test -f .env || cp .env.example .env')
+  print "OK DONE! Hello? Are you still awake?\nEdit your .env file with local credentials\nRun `yarn start` to compile & watch assets"
 
 def deploy(composer='y', assets='y'):
   update()
