d30f46246a933e005cccdd44af412b0aa358b420
Nate Beaty <nate@clixel.com>
2020-02-04 11:32:32 -0500

    only allow single accordion to be open, remove query string when opening an accordion, remove hash when collapsing open accordion

--- assets/scripts/routes/careers.js
+++ assets/scripts/routes/careers.js
@@ -5,40 +5,47 @@ import appState from '../util/appState';
 const careers = {
 
   init() {
-
     // Position accordions
     $('.accordion').each(function() {
       let $this = $(this);
-      let href = $this.find('a')[0].href;
       $this.on('click', (e) => {
-        if (!$this.hasClass('-active')) {
+        if (!appState.isAnimating && !$this.hasClass('-active')) {
           e.preventDefault();
-          history.pushState(null, null, href);
+          // Push URL+hash for Ariel
+          history.pushState(null, null, location.pathname + '#' + $this.data('position'));
           careers.openAccordion($this);
         }
       });
       $this.find('a.collapse').on('click', (e) => {
         e.preventDefault();
         appState.isAnimating = true;
+        // Reset URL for Ariel
+        history.pushState(null, null, location.pathname);
         $this
           .removeClass('-active')
           .find('.description')
           .velocity('slideUp', 500, 'easeOutCubic', () => {
             appState.isAnimating = false
           });
-        // history.back();
       });
     });
 
     // Watch for state change
     window.addEventListener('popstate', careers.checkAccordion);
-
   },
 
   openAccordion($accordion) {
     if ($accordion.hasClass('-active') || appState.isAnimating) {
       return false;
     }
+
+    // Collapse open accordions
+    $('.accordion.-active').each(function() {
+      $(this).removeClass('-active').find('.description').velocity('slideUp', 0, () => {
+        $accordion.velocity('scroll', { duration: 150, offset: -document.querySelector('.site-header').offsetHeight });
+      });
+    });
+
     appState.isAnimating = true;
     $accordion.addClass('-active');
     $accordion.find('.description').velocity('slideDown', 500, 'easeOutCubic', () => appState.isAnimating = false);
@@ -53,19 +60,12 @@ const careers = {
       });
   },
 
-  openPerson($person) {
-    $('body').removeClass('-cursor-active');
-    let html = $person.find('.modal-content').html();
-    let hash = $person.attr('data-person');
-    modals.openModal(html, hash);
-  },
-
   checkAccordion() {
     if (location.hash) {
-      let $position = $(`[data-position="${window.location.hash.replace('#','')}"]`);
+      let $position = $(`[data-position="${location.hash.replace('#','')}"]`);
       if ($position.length) {
+        $position.velocity('scroll', { duration: 150, offset: -document.querySelector('.site-header').offsetHeight });
         careers.openAccordion($position);
-        $position.velocity('scroll', { duration: 500, offset: -document.querySelector('.site-header').offsetHeight });
       }
     } else {
       careers.closeAccordions();
@@ -73,8 +73,6 @@ const careers = {
   },
 
   finalize() {
-    // JavaScript fired after the init JS
-
     // Check for initial window.hash (set in main.js before swup init) on pageload to open modal
     if (appState.initialHash) {
       // Push carousel hash into history
