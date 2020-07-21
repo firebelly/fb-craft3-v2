91a2552905dd3195a5c743c954cc54ffc01bbb2c
Nate Beaty <nate@clixel.com>
2020-07-21 15:18:58 -0400

    nm roll back user check in config

--- config/imager.php
+++ config/imager.php
@@ -3,6 +3,6 @@ return [
   '*' => [
     'cacheDuration' => 31536000, // cache for a year
     'resizeFilter' => 'catrom', // quicker transforms
-    'imagerUrl' => (Craft::$app->getUser() ? '/imager/' : '@cdnUrl/imager/'),
+    'imagerUrl' => '@cdnUrl/imager/',
   ],
 ];
