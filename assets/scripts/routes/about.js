5db9680fcbb0dd44e09a88d0a1cccd20848126f7
Nate Beaty <nate@clixel.com>
2020-09-21 15:20:47 -0400

    add comment

--- assets/scripts/routes/about.js
+++ assets/scripts/routes/about.js
@@ -30,6 +30,7 @@ const about = {
         if (appState.isAnimating) {
           return;
         }
+        // Find first link in article.person to push to history
         let href = this.querySelector('a').href;
         history.pushState(null, null, href);
         about.openPerson($person);
