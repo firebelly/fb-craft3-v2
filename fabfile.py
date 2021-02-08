33c111d0fef132ea7c67cd7285229862d653043c
Nate Beaty <nate@clixel.com>
2021-02-08 18:17:35 -0500

    update to fabric 2 for deploy script

--- fabfile.py
+++ fabfile.py
@@ -1,38 +1,42 @@
-from fabric.api import *
-import os
+from fabric import task
+from invoke import run as local
+from patchwork.transfers import rsync
 
-env.hosts = ['stage.firebelly.co']
-env.user = 'firebelly'
-env.remotepath = '/home/firebelly/apps/fb-craft-staging'
-env.git_branch = 'staging'
-env.warn_only = True
-env.forward_agent = True
+remote_path = "/home/firebelly/apps/fb-craft-staging"
+remote_hosts = ["firebelly@stage.firebelly.co"]
+git_branch = "staging"
+php_command = "php74"
 
-def production():
-  env.hosts = ['firebellydesign.com']
-  env.git_branch = 'main'
-  env.remotepath = '/home/firebelly/apps/fb-craft'
+# set to production
+@task
+def production(c):
+  global remote_hosts, remote_path, git_branch
+  remote_hosts = ["firebelly@firebellydesign.com"]
+  git_branch = "main"
+  remote_path = "/home/firebelly/apps/fb-craft"
 
-# def syncstaging():
-#   with cd(env.remotepath):
-#     run('/usr/bin/mysqldump --defaults-extra-file=/home/firebelly/etc/.my.cnf -u fb_craft_sql fb_craft3 | /usr/bin/mysql --defaults-extra-file=/home/firebelly/etc/.my.cnf -u fb_craft_sql fb_craft3_dev')
-#     run('/usr/bin/rsync -av --delete /home/firebelly/webapps/fb_craft3/web/uploads/ /home/firebelly/webapps/fb_craft3_dev/web/uploads/')
+# deploy
+@task(hosts=remote_hosts)
+def deploy(c, assets="y"):
+  update(c)
+  composer_install(c)
+  # `fab deploy --assets=n` to skip assets
+  if assets == "y":
+    build_assets(c)
+  clear_cache(c)
 
-def deploy(composer='y', assets='y'):
-  update()
-  if composer == 'y':
-    composer_install()
-  # build and sync production assets
-  if assets != 'n':
-    local('rm -rf web/assets/dist')
-    local('yarn build:production')
-    run('mkdir -p ' + env.remotepath + '/web/assets/dist')
-    put('web/assets/dist', env.remotepath + '/web/assets/')
+def update(c):
+  c.run("cd {} && git pull origin {}".format(remote_path, git_branch))
 
-def update():
-  with cd(env.remotepath):
-    run('git pull origin {0}'.format(env.git_branch))
+def composer_install(c):
+  c.run("cd {} && {} ~/bin/composer.phar install".format(remote_path, php_command))
 
-def composer_install():
-  with cd(env.remotepath):
-    run('php74 ~/bin/composer.phar install')
+def build_assets(c):
+  local("rm -rf web/assets/dist")
+  local("yarn build:production")
+  c.run("mkdir -p {}/web/assets/dist".format(remote_path))
+  rsync(c, "web/assets/dist", "{}/web/assets/".format(remote_path))
+
+def clear_cache(c):
+  c.run("cd {} && ./craft clear-caches/compiled-templates".format(remote_path))
+  c.run("cd {} && ./craft clear-caches/data".format(remote_path))
